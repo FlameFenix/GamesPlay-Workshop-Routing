29143f57895efb763a1502da1eae78c0595ffdad
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-07-18 23:18:08 +0300

    Added latest games from server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import './App.css';
+
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
-import { Routes, Route } from 'react-router-dom'
+
 import { CreateGame } from './components/CreateGame/CreateGame';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { Catalogue } from './components/Catalogue/Catalogue';
+import { GameDetails } from './components/GameDetails/GameDetails';
+
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+
+import * as gameService from './services/gameService'
 
 function App() {
+
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    gameService.getAll()
+      .then(games => setGames(games))
+  }, [])
+
   return (
     <div>
 
       <Header />
 
       <main id="main-content">
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="catalogue" element={<Catalogue />} />
+          <Route path="/" element={<Home games={games} />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/catalogue/:gameId" element={<GameDetails />} />
           <Route path="/create" element={<CreateGame />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
