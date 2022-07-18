0e364759ea43b14924caef7cebd810661436bf5a
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2022-07-18 22:53:26 +0300

    Routing added to navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+import { Routes, Route } from 'react-router-dom'
+import { CreateGame } from './components/CreateGame/CreateGame';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { Catalogue } from './components/Catalogue/Catalogue';
 
 function App() {
   return (
@@ -9,7 +14,15 @@ function App() {
       <Header />
 
       <main id="main-content">
-        <Home />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="catalogue" element={<Catalogue />} />
+          <Route path="/create" element={<CreateGame />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+
       </main>
 
     </div>
